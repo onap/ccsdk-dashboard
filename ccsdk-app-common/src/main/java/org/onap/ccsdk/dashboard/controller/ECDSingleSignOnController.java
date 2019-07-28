6c01248f330a01a2f605474db632ae5d011b2e31
Thugutla Sailakshmi <tsaila10@in.ibm.com>
2019-07-28 19:29:25 +0530

    CCSDK-1549 -changing name of varaible to remove confusion in hierarcy
    changing name of varaible to remove confusion in hierarcy
    
    Issue-ID: CCSDK-1549
    Change-Id: Iaeb9fd321b134a4615aa5b6667a66ade7229db81
    Signed-off-by: Thugutla Sailakshmi <tsaila10@in.ibm.com>

--- ccsdk-app-common/src/main/java/org/onap/ccsdk/dashboard/controller/ECDSingleSignOnController.java
+++ ccsdk-app-common/src/main/java/org/onap/ccsdk/dashboard/controller/ECDSingleSignOnController.java
@@ -3,6 +3,8 @@
  *
  * =================================================================================
  *  Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+ *  
+ *  Modifications Copyright (C) 2019 IBM.
  * ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -96,7 +98,7 @@ public class ECDSingleSignOnController extends UnRestrictedBaseController {
     @Autowired
     private RoleService roleService;
 
-    private String viewName;
+    private String NameofView;
     private String welcomeView;
 
     /**
@@ -271,12 +273,12 @@ public class ECDSingleSignOnController extends UnRestrictedBaseController {
         return request.getSession().getId();
     }
 
-    public String getViewName() {
-        return viewName;
+    public String getNameofView() {
+        return NameofView;
     }
 
-    public void setViewName(String viewName) {
-        this.viewName = viewName;
+    public void setNameofView(String NameofView) {
+        this.NameofView = NameofView;
     }
 
     public String getWelcomeView() {
